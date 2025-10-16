83cac38f46076492decde5fc82e0bc2310802c19
Marquis Ogre <marquisogre@gmail.com>
2025-10-16 11:23:38 +0530

    Update pricingData.ts

--- src/data/pricingData.ts
+++ src/data/pricingData.ts
@@ -1,5 +1,20 @@
 
-import { PackingData } from "./types";
+import { PricingData, PackingData } from "./types";
+
+export const productPricesData: PricingData[] = [
+  { id: 1, product: "Phenyl", uom: "1 Ltr", retailPrice: 60, bulkPrice5Ltr: 250 },
+  { id: 2, product: "Dish Wash Liquid", uom: "500 ML", retailPrice: 80, bulkPrice5Ltr: 750 },
+  { id: 3, product: "Copper/Brass/Steel Liquid", uom: "500 ML", retailPrice: 100, bulkPrice5Ltr: 900 },
+  { id: 4, product: "Harpic", uom: "1 Ltr", retailPrice: 100, bulkPrice5Ltr: 450 },
+  { id: 5, product: "Acid", uom: "1 Ltr", retailPrice: 50, bulkPrice5Ltr: 200 },
+  { id: 6, product: "Hand Wash Liquid", uom: "500 ML", retailPrice: 70, bulkPrice5Ltr: 650 },
+  { id: 7, product: "Detergent Powder", uom: "1 Kg", retailPrice: 150, bulkPrice5Ltr: 700 },
+  { id: 8, product: "Liquid Detergent", uom: "1 Ltr", retailPrice: 100, bulkPrice5Ltr: 450 },
+  { id: 9, product: "Floor Cleaning Liquid", uom: "1 Ltr", retailPrice: 100, bulkPrice5Ltr: 450 },
+  { id: 10, product: "Rose Water", uom: "1 Ltr", retailPrice: 60, bulkPrice5Ltr: 250 },
+  { id: 11, product: "Pain Relief Balm (Zandu Balm)", uom: "25 Gms", retailPrice: 80, bulkPrice100Gms: 280 },
+  { id: 12, product: "White Petroleum Jelly (Vaseline)", uom: "25 Gms", retailPrice: 25, bulkPrice100Gms: 75 }
+];
 
 export const packingMaterialsData: PackingData[] = [
   { id: 1, product: "Acid Bottles - 1 Ltr", minimumOrder: 105, retailPrice: 4.3 },
